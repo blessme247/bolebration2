3f2b00289899b741b9c825c82c78212d4191e917
Abraham Solabi <solabiabraham@gmail.com>
2023-04-28 13:09:35 +0100

    Fixed scanner styling

--- src/Pages/QRScanner/QRScanner.jsx
+++ src/Pages/QRScanner/QRScanner.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useRef, useState } from "react";
 import QrScanner from "qr-scanner";
 import Swal from "sweetalert2";
-
-import "./scanner.scss";
+import { motion } from "framer-motion";
 import axiosInstance from "../../utils/axiosConfig";
+import "./scanner.scss";
 
 const QRScanner = () => {
   const videoRef = useRef();
@@ -15,19 +15,10 @@ const QRScanner = () => {
 
   useEffect(() => {}, [videoRef]);
 
-  const showOutput = (result) => {
-    Swal.fire({
-      position: "center",
-      icon: "info",
-      title: result,
-      showConfirmButton: true,
-      timer: 3500,
-    });
-  };
-
   const verifyQR = async (data) => {
-    //If resultText state has not been set yet, use a parsed result.data on line 59 directly
-    let payload = resultText || data;
+    //If resultText state has not been set yet, use a parsed result.data on line 60 directly
+    // let payload = resultText || data;
+    let payload = data;
 
     let response = await axiosInstance.post("/scanned", payload);
 
@@ -37,8 +28,19 @@ const QRScanner = () => {
         icon: response.data == "scanned" ? "success" : "error",
         title: response.data,
         showConfirmButton: true,
+        timer: 15000,
+      })
+      
+    }
+
+    else {
+      Swal.fire({
+        position: "center",
+        icon: "error",
+        title: "Something Went Wrong!",
+        showConfirmButton: true,
         timer: 3500,
-      });
+      })
     }
   };
 
@@ -48,13 +50,12 @@ const QRScanner = () => {
     qrScanner = new QrScanner(
       videoRef.current,
       (result) => {
-        console.log(result, "whole result object");
-        // showOutput(result.data);
+        // console.log(result, "whole result object");
         setResultText(() => {
           return JSON.parse(result.data);
         });
         setShowResultText(true);
-        console.log("decoded qr code:", resultText);
+        // console.log("decoded qr code:", resultText);
         // console.log("decoded qr code email:", result?.data.email);
         qrScanner.stop();
         verifyQR(JSON.parse(result.data));
@@ -63,7 +64,6 @@ const QRScanner = () => {
         returnDetailedScanResult: true,
         highlightScanRegion: true,
         onDecodeError: (err) => {
-          //   console.log(err);
         },
         maxScansPerSecond: undefined,
         /* your options or returnDetailedScanResult: true if you're not specifying any other options */
@@ -74,27 +74,37 @@ const QRScanner = () => {
 
   const scanStart = () => {
     // if(videoRef != null) {
+      setResultText()
     qrScanner?.start();
     // }
   };
   return (
     <div className="qrPageContainer">
-      <div style={{ margin: "0 auto", width: "800px" }}>
+      <div className="qrPageWrapper" >
         {/* <QrScanner /> */}
 
         <video className="video" ref={videoRef} src={videoRef.current}></video>
         <button className="scanBtn" onClick={scanStart}>
-          Scan{" "}
+          Scan 
         </button>
 
         {showResultText && resultText && (
 
-        <div className="scanResultTitle">QR Code Details</div>
+        <motion.div className="scanResultTitle" initial={{opacity: 0, y: 80}} animate={{opacity: 1, y: 0}}
+        transition= {{
+         ease: "easeInOut",
+         duration: 0.8,
+        }} >QR Code Details</motion.div>
 
         )}
 
         {showResultText && resultText && (
-          <div className="scanResult">
+          <motion.div className="scanResult" initial={{opacity: 0, y: 80}} animate={{opacity: 1, y: 0}}
+          transition= {{
+           ease: "easeInOut",
+           duration: 1,
+           delay: 0.4
+          }}>
             <p>Email: {resultText?.email} </p>
             <p>Phone: {resultText?.phone} </p>
             <p>Name: {resultText?.name} </p>
@@ -107,7 +117,7 @@ const QRScanner = () => {
                 <p>Ticket Type: {resultText.ticketType} </p>
               </>
             )}
-          </div>
+          </motion.div>
         )}
       </div>
     </div>

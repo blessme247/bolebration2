61df6035e3adf530d09a97555530dbd0f3b5effc
Abraham Solabi <solabiabraham@gmail.com>
2023-05-06 05:18:27 +0100

    fix image import

--- src/Pages/Homepage/SwiperSlides.jsx
+++ src/Pages/Homepage/SwiperSlides.jsx
@@ -10,7 +10,7 @@ import "swiper/css/navigation";
 import image1 from "../../assets/Images/image1JPG.jpg"
 import image2 from "../../assets/Images/Image2JPG.jpg"
 import image3 from "../../assets/Images/image3JPG.jpg"
-import image4 from "../../assets/Images/image4JPG.jpg"
+import image4 from "../../assets/Images/Image4JPG.jpg"
 import image5 from "../../assets/Images/image5JPG.jpg"
 import image6 from "../../assets/Images/image6JPG.jpg"
 

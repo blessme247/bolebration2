f8e3bead942bb8e8dba9d951ff8ede3ae438c660
Abraham Solabi <solabiabraham@gmail.com>
2023-05-07 10:57:32 +0100

    Testing Imagekit CDN

--- src/Pages/Order/food.js
+++ src/Pages/Order/food.js
@@ -1,4 +1,4 @@
-import food1 from "../../assets/Images/menu1.jpg"
+// import food1 from "../../assets/Images/menu1.jpg"
 import food2 from "../../assets/Images/menu2.jpg"
 import food3 from "../../assets/Images/menu3.jpg"
 import food4 from "../../assets/Images/menu4.jpg"
@@ -8,7 +8,7 @@ import food6 from "../../assets/Images/menu6.jpg"
 
 // database of food items on the order page 
 export const foodTray = [
-    {id : "f1", name: "Plantain Boat", price: "₦1,000", imageSrc: food1},
+    {id : "f1", name: "Plantain Boat", price: "₦1,000", imageSrc: "https://ik.imagekit.io/blessme247/menu1.jpg?updatedAt=1683453159937"},
     {id : "f2", name: " Bole and Chicken", price: "₦3,000", imageSrc: food2},
     {id : "f3", name: " Bole and Turkey", price: "₦3,500", imageSrc: food3},
     {id : "f4", name: "Bole and Mackerel fish", price: "₦2,500", imageSrc: food4},
